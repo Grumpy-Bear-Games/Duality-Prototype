8136ded3bb9b0a0be10b0d9534c72690789da99f
Martin Parm <parmus@gmail.com>
2022-01-29 00:34:16 +0100

    First prototype added

---
+++ Assets/Scripts/Realm.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace DualityGame
+{
+    public class Realm : MonoBehaviour
+    {
+        [SerializeField] private LayerMask _levelLayers;
+        [SerializeField] private int _playerLayer;
+
+        public LayerMask LeveLayers => _levelLayers;
+        public int PlayerLayer => _playerLayer;
+
+        private bool _active;
+
+        public bool Active
+        {
+            get => _active;
+            set
+            {
+                _active = value;
+                foreach (var renderer in GetComponentsInChildren<Renderer>())
+                {
+                    renderer.enabled = _active;
+                }
+            }
+        }
+    }
+}
