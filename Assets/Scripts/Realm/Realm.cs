69d6f8b1615455e78308e3857b5caf312f64e105
Martin Parm <parmus@gmail.com>
2022-01-29 10:26:35 +0100

    Refactored Realm management into scriptable objects and prepare navigation

---
+++ Assets/Scripts/Realm/Realm.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace DualityGame.Realm
+{
+    [CreateAssetMenu(fileName = "Realm", menuName = "Duality/Realm", order = 0)]
+    public class Realm : ScriptableObject
+    {
+        [SerializeField] private LayerMask _levelLayers;
+        [SerializeField] private int _playerLayer;
+
+        public LayerMask LeveLayers => _levelLayers;
+        public int PlayerLayer => _playerLayer;
+    }
+}
