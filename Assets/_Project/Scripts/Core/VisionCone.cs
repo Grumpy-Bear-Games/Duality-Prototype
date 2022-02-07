5610ad5f03c94b4c8d14d02629fd3bd8ba91afaa
Martin Parm <parmus@gmail.com>
2022-02-07 23:54:50 +0100

    Add basic vision cone

---
+++ Assets/_Project/Scripts/Core/VisionCone.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DualityGame.Core
+{
+    [RequireComponent(typeof(MeshFilter))]
+    [RequireComponent(typeof(MeshRenderer))]
+    public class VisionCone : MonoBehaviour
+    {
+        [SerializeField] private float _range = 1f;
+        [SerializeField, Range(0, 180)] private float _fov = 45;
+        [SerializeField, Delayed, Range(1, 60)] private int _precision = 2;
+        [SerializeField] private LayerMask _layerMask;
+
+        private Transform _transform;
+        private MeshFilter _meshFilter;
+        private float _timer;
+        private readonly List<Vector3> _originalVertices = new();
+    
+        private void Awake()
+        {
+            _transform = transform;
+            _meshFilter = GetComponent<MeshFilter>();
+            if (_meshFilter.mesh == null) _meshFilter.mesh = new Mesh();
+            InitMesh(_meshFilter.mesh);
+        }
+
+
+        private void LateUpdate() => UpdateVisionCone(_meshFilter.mesh);
+
+        private void InitMesh(Mesh mesh)
+        {
+            if (_precision < 1) return;
+        
+            _originalVertices.Clear();
+        
+            var triangles = new List<int>();
+            var normals = new List<Vector3>();
+            var uv = new List<Vector2>();
+        
+            _originalVertices.Add(Vector3.zero);
+            normals.Add(Vector3.up);
+            uv.Add(Vector2.zero);
+
+            var dir = Vector3.forward * _range;
+        
+            for (var i = 0; i < _precision + 1; i++)
+            {
+                var start = -_fov / 2;
+
+                _originalVertices.Add(Quaternion.AngleAxis(start + i * (_fov / _precision), Vector3.up) * dir);
+                normals.Add(Vector3.up);
+                uv.Add(Vector2.right);
+            }
+
+            for (var i = 0; i < _precision; i++)
+            {
+                triangles.Add(0);
+                triangles.Add(i + 1);
+                triangles.Add(i + 2);
+            }
+
+            mesh.Clear();
+            mesh.vertices = _originalVertices.ToArray();
+            mesh.normals = normals.ToArray();
+            mesh.uv = uv.ToArray();
+            mesh.triangles = triangles.ToArray();
+        }
+
+        private void OnValidate()
+        {
+#if UNITY_EDITOR
+            if (UnityEditor.EditorApplication.isPlayingOrWillChangePlaymode) return;
+#endif
+            _transform = transform;
+            _meshFilter = GetComponent<MeshFilter>();
+            if (_meshFilter.sharedMesh == null) _meshFilter.sharedMesh = new Mesh();
+
+            InitMesh(_meshFilter.sharedMesh);
+            UpdateVisionCone(_meshFilter.sharedMesh);
+        }
+
+        private void UpdateVisionCone(Mesh mesh)
+        {
+            var pos = _transform.position;
+            var vertices = new List<Vector3> { Vector3.zero };
+            var uv = new List<Vector2> { Vector2.zero };
+            for (var i=0; i<_originalVertices.Count; i++) {
+                var vertex = _originalVertices[i];
+                if (i == 0)
+                {
+                    vertices.Add(vertex);
+                    uv.Add(Vector2.zero);
+                    continue;
+                }
+
+                var ray = _transform.rotation * vertex;
+                if (Physics.Raycast(pos, ray, out var hit, _range, _layerMask))
+                {
+                    vertices.Add(vertex.normalized * hit.distance);
+                    uv.Add(Vector2.right * (hit.distance / _range));
+                    if (hit.transform.CompareTag("Player"))
+                    {
+                        Debug.Log("Player spotted!");
+                    }
+                }
+                else
+                {
+                    vertices.Add(vertex);
+                    uv.Add(Vector2.right);
+                }
+            }
+
+            mesh.vertices = vertices.ToArray();
+            mesh.uv = uv.ToArray();
+            mesh.RecalculateBounds();
+        }
+    }
+}
