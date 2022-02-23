834eb2a519f32e3f8cd2692e93a60a603b81691e
Martin Parm <parmus@gmail.com>
2022-02-23 22:44:24 +0100

    Adjust menu settings

--- Assets/_Project/Scripts/UI/MainMenu.cs
+++ Assets/_Project/Scripts/UI/MainMenu.cs
@@ -5,10 +5,6 @@ namespace DualityGame.UI
 {
     public class MainMenu : MonoBehaviour
     {
-        [SerializeField] private UnityEngine.UI.Selectable _firstSelected;
-
-        private void Start() => _firstSelected.Select();
-
         // Temporary, very naive approach
         public void NewGame() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
