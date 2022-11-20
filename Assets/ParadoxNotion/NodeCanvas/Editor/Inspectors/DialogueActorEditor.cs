3a04bfeaf505712327be4dfa8ba3c973eab70c53
Martin Parm <parmus@gmail.com>
2022-11-20 23:37:56 +0100

    Custom editor for DialogueActor

---
+++ Assets/ParadoxNotion/NodeCanvas/Editor/Inspectors/DialogueActorEditor.cs
@@ -0,0 +1,39 @@
+﻿using NodeCanvas.DialogueTrees;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace NodeCanvas.Editor
+{
+    [CustomEditor(typeof(DialogueActor))]
+    public class DialogueActorEditor: UnityEditor.Editor
+    {
+        [SerializeField] private VisualTreeAsset _editor;
+        [SerializeField] private VisualTreeAsset _portraitItem;
+        
+        public override VisualElement CreateInspectorGUI()
+        {
+            var root = _editor.CloneTree();
+            var portraitsListView = root.Q<ListView>();
+            portraitsListView.makeItem = MakeItem;
+            return root;
+        }
+
+        private VisualElement MakeItem()
+        {
+            var root = _portraitItem.CloneTree();
+
+            var portraitPropertyField = root.Q<PropertyField>("Portrait");
+            var preview = root.Q<VisualElement>("SpritePreview");
+            portraitPropertyField.RegisterValueChangeCallback(e =>
+            {
+                var sprite = e.changedProperty.objectReferenceValue as Sprite;
+                preview.style.backgroundImage = sprite != null
+                    ? new StyleBackground(sprite)
+                    : new StyleBackground(StyleKeyword.Initial);
+            });
+            return root;
+        }
+    }
+}
