b543d440ee0feb30dc05ba0d6c0962146e46daab
Martin Parm <parmus@gmail.com>
2022-11-22 23:53:35 +0100

    Add ItemType custom editor

---
+++ Assets/_Project/Scripts/Editor/ItemTypeEditor.cs
@@ -0,0 +1,33 @@
+﻿using DualityGame.Inventory;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace DualityGame.Editor
+{
+    [CustomEditor(typeof(ItemType))]
+    public class ItemTypeEditor: UnityEditor.Editor
+    {
+        [SerializeField] private VisualTreeAsset _editor;
+        private VisualElement _preview;
+
+        public override VisualElement CreateInspectorGUI()
+        {
+            var root = _editor.CloneTree();
+            root.Q<Label>("Name").text = target.name;
+
+            _preview = root.Q<VisualElement>("InventorySpritePreview");
+            root.Q<ObjectField>("InventorySprite")
+                .RegisterValueChangedCallback(evt => UpdatePreview(evt.newValue as Sprite));
+            if (target is ItemType itemType) UpdatePreview(itemType.InventorySprite);
+            return root;
+        }
+
+        private void UpdatePreview(Sprite sprite)
+        {
+            _preview.style.backgroundImage = new StyleBackground(sprite);
+            _preview.style.display = sprite != null ? DisplayStyle.Flex : DisplayStyle.None;
+        }
+    }
+}
