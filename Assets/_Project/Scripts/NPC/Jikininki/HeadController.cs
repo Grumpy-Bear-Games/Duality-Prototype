35640ce4b89ea82f2992c8ac77dd667b288bac42
Martin Parm <parmus@gmail.com>
2022-12-02 11:30:03 +0100

    Make Jikininki head controller more stable

--- Assets/_Project/Scripts/NPC/Jikininki/HeadController.cs
+++ Assets/_Project/Scripts/NPC/Jikininki/HeadController.cs
@@ -1,31 +1,42 @@
-﻿using DualityGame.Realm;
+﻿using System;
+using DualityGame.Realm;
 using UnityEngine;
 
 namespace DualityGame.NPC.Jikininki
 {
     public class HeadController : MonoBehaviour
     {
+        private static readonly int PlayerCloseProperty = Animator.StringToHash("Head High");
+
         [SerializeField] private Animator _animator;
         [SerializeField] private RealmObservable _currentRealm;
         [SerializeField] private float _maxDistance = 5f;
         
         private GameObject _player;
-        private static readonly int PlayerCloseProperty = Animator.StringToHash("Head High");
 
+        public void SetPlayer(GameObject player)
+        {
+            _player = player;
+            enabled = _player != null;
+        }
 
-        private void Awake() => _player = GameObject.FindWithTag("Player");
-
+        private void Awake() => enabled = _player != null;
+        
         private void Update() => _animator.SetBool(PlayerCloseProperty, IsPlayerClose());
 
         private bool IsPlayerClose() => (
+            (_player != null) &&
             (_currentRealm.Value.LevelLayer == gameObject.layer) &&
             (Vector3.Distance(transform.position, _player.transform.position) <= _maxDistance)
         );
-
+        
         #if UNITY_EDITOR
-        private void Reset()
+        private void Reset() => _animator = GetComponentInChildren<Animator>();
+
+        private void OnDrawGizmosSelected()
         {
-            _animator = GetComponentInChildren<Animator>();
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, _maxDistance);
         }
         #endif
     }
