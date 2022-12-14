d48ca2e6de2a9940102696eadf16e853750d868b
Martin Parm <parmus@gmail.com>
2022-12-14 22:43:24 +0100

    Quest Log (part 1)

---
+++ Assets/_Project/Scripts/Quests/UI/QuestLog.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace DualityGame.Quests.UI
+{
+    [RequireComponent(typeof(UIDocument))]
+    public class QuestLog : MonoBehaviour
+    {
+        private UIDocument _uiDocument;
+
+        private QuestSlot leftSlot;
+        private QuestSlot rightSlot;
+
+        [SerializeField] private List<Quest> _quests; 
+
+        private void Awake()
+        {
+            _uiDocument = GetComponent<UIDocument>();
+
+            var pages = _uiDocument.rootVisualElement.Query<QuestSlot>().ToList();
+            leftSlot = pages[0];
+            rightSlot = pages[1];
+
+            leftSlot.Quest = _quests[0];
+            rightSlot.Quest = _quests[1];
+        }
+    }
+}
