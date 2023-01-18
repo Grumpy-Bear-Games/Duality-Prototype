0381ddf96561230f3c5b772dcd9c65c970cb4c60
Martin Parm <parmus@gmail.com>
2023-01-18 22:54:16 +0100

    Update Quest class
    - Add support for NPC, Realm and visibility
    - Add static set to access all Quests
    - Add RevealQuest NodeCanvas task

--- Assets/_Project/Scripts/Quests/Quest.cs
+++ Assets/_Project/Scripts/Quests/Quest.cs
@@ -1,4 +1,7 @@
-﻿using UnityEditor;
+﻿using System;
+using System.Collections.Generic;
+using NodeCanvas.DialogueTrees;
+using UnityEditor;
 using UnityEngine;
 
 namespace DualityGame.Quests
@@ -6,32 +9,75 @@ namespace DualityGame.Quests
     [CreateAssetMenu(fileName = "Quest", menuName = "Duality/Quest", order = 0)]
     public class Quest : ScriptableObject
     {
-        [SerializeField] private string _title;
-        [SerializeField] private string _description;
-
-        private QuestState _state = QuestState.Inactive;
-
-        public string Title => _title;
-        public string Description => _description;
+        #region Serialized fields
+        [field: SerializeField] public string Title { get; private set; }
+        [field: Multiline][field: SerializeField] public string Description { get; private set; }
+        [field: SerializeField] public ActorAsset NPC { get; private set; }
+        [field: SerializeField] public Realm.Realm Realm { get; private set; }
+        [field: SerializeField] public QuestVisibility Visibility { get; private set; } =
+            QuestVisibility.ShowAutomaticallyWhenOngoing;
+        #endregion
         
+        #region Public properties
         public QuestState State
         {
             get => _state;
-            set => _state = value;
+            set
+            {
+                if (_state == value) return;
+                _state = value;
+                if (_state == QuestState.Ongoing) QuestStartedTimestamp = DateTime.Now.Ticks;
+                if (Visibility == QuestVisibility.ManualVisibility) return;
+                Hidden = (_state == QuestState.Inactive);
+            }
         }
 
+        public bool Hidden { get; private set; } = true;
+
+        public long QuestStartedTimestamp { get; private set; } = 0;
+        #endregion
+
+        
+        #region Private fields
+        private QuestState _state = QuestState.Inactive;
+        #endregion
+
+
+        #region Public methods
+        public void Reveal() => Hidden = false;
+        #endregion
+
+
+        #region Public enums
         public enum QuestState
         {
-            Inactive, Ongoing, Completed, Failed
+            Inactive,
+            Ongoing,
+            Completed,
+            Failed,
         }
 
-#if UNITY_EDITOR
+        public enum QuestVisibility
+        {
+            ShowAutomaticallyWhenOngoing,
+            ManualVisibility
+        }
+        #endregion
+
+
+        public static readonly HashSet<Quest> AllQuests = new();
+
+        #region Life-cycle management
         private void OnEnable()
         {
+            AllQuests.Add(this);
+#if UNITY_EDITOR
             if (!EditorApplication.isPlayingOrWillChangePlaymode) return;
-            _state = QuestState.Inactive;
-        }
+            State = QuestState.Inactive;
+            Hidden = true;
+            QuestStartedTimestamp = 0;
 #endif
-
+        }
+        #endregion
     }
 }
--- Assets/_Project/Scripts/Quests/RevealQuest.cs
+++ Assets/_Project/Scripts/Quests/RevealQuest.cs
@@ -4,20 +4,19 @@ using ParadoxNotion.Design;
 namespace DualityGame.Quests
 {
     [Category("Duality")]
-    [Description("Update quest")]
-    public class UpdateQuest : ActionTask
+    [Description("Reveal quest")]
+    public class RevealQuest : ActionTask
     {
         [RequiredField] public BBParameter<Quest> _quest;
-        [RequiredField] public BBParameter<Quest.QuestState> _questState;
 
-        protected override string info => _quest.isNoneOrNull ? "(Please specify quest)" : $"Set quest state to {_questState.value}";
+        protected override string info => _quest.isNoneOrNull ? "(Please specify quest)" : "Reveal quest";
 
         //This is called once each time the task is enabled.
         //Call EndAction() to mark the action as finished, either in success or failure.
         //EndAction can be called from anywhere.
         protected override void OnExecute()
         {
-            _quest.value.State = _questState.value;
+            _quest.value.Reveal();
             EndAction(true);
         }
     }
