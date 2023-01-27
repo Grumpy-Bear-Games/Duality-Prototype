7f4865b486e3b372767668d586451ccc755d3e87
Martin Parm <parmus@gmail.com>
2023-01-27 23:55:27 +0100

    Handle SaveableEntity ID uniqueness

--- Assets/_Project/Scripts/SaveSystem/SaveableEntity.cs
+++ Assets/_Project/Scripts/SaveSystem/SaveableEntity.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 namespace DualityGame.SaveSystem
 {
+    [ExecuteAlways]
     public class SaveableEntity : MonoBehaviour
     {
+        [Tooltip("The unique ID is automatically generated in a scene file if " +
+                 "left empty. Do not set in a prefab unless you want all instances to " + 
+                 "be linked.")]        
         [SerializeField] private string _id;
 
         public string ID => _id;
@@ -31,5 +36,43 @@ namespace DualityGame.SaveSystem
 
         private static string GetComponentID(ISaveableComponent saveableComponent) =>
             saveableComponent.GetType().ToString();
+        
+#if UNITY_EDITOR
+        private static readonly Dictionary<string, SaveableEntity> globalLookup = new();
+        
+        private void Update() {
+            if (Application.IsPlaying(gameObject)) return;
+            if (string.IsNullOrEmpty(gameObject.scene.path)) return;
+
+            var serializedObject = new SerializedObject(this);
+            var property = serializedObject.FindProperty("_id");
+            
+            if (string.IsNullOrEmpty(property.stringValue) || !IsUnique(property.stringValue))
+            {
+                property.stringValue = System.Guid.NewGuid().ToString();
+                serializedObject.ApplyModifiedProperties();
+            }
+
+            globalLookup[property.stringValue] = this;
+        }
+
+        private bool IsUnique(string candidate)
+        {
+            if (!globalLookup.ContainsKey(candidate)) return true;
+
+            if (globalLookup[candidate] == this) return true;
+
+            if (globalLookup[candidate] == null)
+            {
+                globalLookup.Remove(candidate);
+                return true;
+            }
+
+            if (globalLookup[candidate]._id == candidate) return false;
+            globalLookup.Remove(candidate);
+            return true;
+
+        }
+#endif
     }
 }
