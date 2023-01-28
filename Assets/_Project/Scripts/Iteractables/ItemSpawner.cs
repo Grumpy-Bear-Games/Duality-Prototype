f12f8cb5812c1e1d28f2bdc1d10e9354583dde9f
Martin Parm <parmus@gmail.com>
2023-01-28 17:05:01 +0100

    Fix spawning animation of save-load

--- Assets/_Project/Scripts/Iteractables/ItemSpawner.cs
+++ Assets/_Project/Scripts/Iteractables/ItemSpawner.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using DG.Tweening;
+using DualityGame.Inventory;
 using DualityGame.SaveSystem;
 using UnityEngine;
 
@@ -8,8 +10,8 @@ namespace DualityGame.Iteractables
     [RequireComponent(typeof(SaveableEntity))]
     public class ItemSpawner : MonoBehaviour, IInteractable, ISaveableComponent
     {
-        [Header("Items to spawn")]
-        [SerializeField] private List<ItemToSpawn> _itemsToSpawn = new();
+        [SerializeField] private Transform _spawnPoint;
+        [SerializeField] private List<Item> _itemsToSpawn = new();
 
         [Header("Interaction prompt")]
         [SerializeField] private string _prompt;
@@ -22,7 +24,7 @@ namespace DualityGame.Iteractables
 
         private bool _hasSpawned;
 
-        private void Awake() => _itemsToSpawn.ForEach(itemToSpawn => itemToSpawn.Item.gameObject.SetActive(false));
+        private void Awake() => _itemsToSpawn.ForEach(itemToSpawn => itemToSpawn.gameObject.SetActive(false));
 
         public void Interact(GameObject actor)
         {
@@ -33,11 +35,13 @@ namespace DualityGame.Iteractables
             
             _itemsToSpawn.ForEach(itemToSpawn =>
             {
-                itemToSpawn.Item.gameObject.SetActive(true);
-                var tween = itemToSpawn.Item.transform
-                    .DOJump(itemToSpawn.SpawnTo.position, _jumpPower, 1, _jumpDuration, false)
+                var spawnTarget = itemToSpawn.transform.position;
+                itemToSpawn.transform.position = _spawnPoint.position;
+                itemToSpawn.gameObject.SetActive(true);
+                var tween = itemToSpawn.transform
+                    .DOJump(spawnTarget, _jumpPower, 1, _jumpDuration, false)
                     .SetEase(_jumpEasing)
-                    .OnComplete(() => itemToSpawn.Item.UpdateInitialPosition());
+                    .OnComplete(itemToSpawn.UpdateInitialPosition);
                 seq.Insert(0f, tween);
             });
         }
@@ -46,15 +50,31 @@ namespace DualityGame.Iteractables
 
         public Vector3 PromptPosition => transform.position + _promptOffset;
 
-        [System.Serializable]
-        public class ItemToSpawn
+
+        private void OnDrawGizmosSelected()
         {
-            public Inventory.Item Item;
-            public Transform SpawnTo;
+            Gizmos.color = Color.green;
+            foreach (var item in _itemsToSpawn)
+            {
+                var itemCollider = item.GetComponent<Collider>();
+                var bounds = itemCollider.bounds;
+                Gizmos.DrawWireCube(bounds.center, bounds.size);
+                Gizmos.DrawLine(transform.position, bounds.center);
+            }
         }
 
         object ISaveableComponent.CaptureState() => _hasSpawned;
 
         void ISaveableComponent.RestoreState(object state) => _hasSpawned = (bool)state;
+        
+        #if UNITY_EDITOR
+        private void OnValidate()
+        {
+            if (_spawnPoint == null)
+            {
+                _spawnPoint = transform;
+            }
+        }
+        #endif
     }
 }
