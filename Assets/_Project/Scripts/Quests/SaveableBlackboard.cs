4095bcd26a5034ea85731450a76cb195c996deda
Martin Parm <parmus@gmail.com>
2023-01-29 19:58:11 +0100

    Add SaveableBlackboard

---
+++ Assets/_Project/Scripts/Quests/SaveableBlackboard.cs
@@ -0,0 +1,19 @@
+﻿using DualityGame.SaveSystem;
+using NodeCanvas.Framework;
+using UnityEngine;
+
+namespace DualityGame.Quests
+{
+    [RequireComponent(typeof(Blackboard))]
+    [RequireComponent(typeof(SaveableEntity))]
+    public class SaveableBlackboard : MonoBehaviour, ISaveableComponent
+    {
+        private Blackboard _blackboard;
+
+        private void Awake() => _blackboard = GetComponent<Blackboard>();
+
+        object ISaveableComponent.CaptureState() => _blackboard.Serialize(null);
+
+        void ISaveableComponent.RestoreState(object state) => _blackboard.Deserialize((string)state, null);
+    }
+}
