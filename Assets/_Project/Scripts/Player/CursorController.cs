d8e3f14f2dc4e13409b91ccc153dec51e070bf49
Martin Parm <parmus@gmail.com>
2023-02-19 00:54:23 +0100

    Implement CursorController

--- Assets/_Project/Scripts/Player/CursorController.cs
+++ Assets/_Project/Scripts/Player/CursorController.cs
@@ -1,15 +1,46 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace DualityGame.Player
 {
-    // TODO: To be fleshed out later
     public class CursorController : MonoBehaviour
     {
         #if !UNITY_IOS || !UNITY_ANDROID
         [Header("Mouse Cursor Settings")]
-        public bool _cursorLocked = true;
-        private void OnApplicationFocus(bool hasFocus) => SetCursorState(_cursorLocked);
-        private static void SetCursorState(bool newState) => Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
+        [SerializeField] private bool _confined = true;
+
+        private bool _hidden;
+        private bool _hasFocus = true;
+
+        private void Awake() => UpdateCursor();
+
+        public void HideCursor(bool hidden)
+        {
+            _hidden = hidden;
+            UpdateCursor();
+        }
+
+        private void UpdateCursor()
+        {
+            if (_hasFocus)
+            {
+                if (_hidden)
+                    Cursor.lockState = CursorLockMode.Locked;
+                else
+                    Cursor.lockState = _confined ? CursorLockMode.Confined : CursorLockMode.None;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.None;
+            }
+        }
+
+
+        private void OnApplicationFocus(bool hasFocus)
+        {
+            _hasFocus = hasFocus;
+            UpdateCursor();
+        }
         #endif
     }
 }
