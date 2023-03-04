d97fc5cb18dd4106c383ebd5fb7479a93750e5c2
Martin Parm <parmus@gmail.com>
2023-03-04 16:00:03 +0100

    Add more tools for creating quests

---
+++ Assets/_Project/Scripts/Utilities/BlackboardUtility.cs
@@ -0,0 +1,30 @@
+﻿using NodeCanvas.Framework;
+using UnityEngine;
+
+namespace DualityGame.Utilities
+{
+    public class BlackboardUtility : MonoBehaviour
+    {
+        private Blackboard _blackboard;
+        private void Awake() => _blackboard = GetComponent<Blackboard>();
+
+        public void SetBooleanTrue(string key) => _blackboard.SetVariableValue(key, true);
+        public void SetBooleanFalse(string key) => _blackboard.SetVariableValue(key, false);
+        public void ToggleBoolean(string key)
+        {
+            var value = _blackboard.GetVariableValue<bool>(key);
+            _blackboard.SetVariableValue(key, !value);
+        }
+
+        public void IncrementInteger(string key)
+        {
+            var value = _blackboard.GetVariableValue<int>(key);
+            _blackboard.SetVariableValue(key, value + 1);
+        }
+        public void DecrementInteger(string key)
+        {
+            var value = _blackboard.GetVariableValue<int>(key);
+            _blackboard.SetVariableValue(key, value - 1);
+        }
+    }
+}
