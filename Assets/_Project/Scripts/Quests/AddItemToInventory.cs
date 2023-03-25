8a4f747ad4a0bc0331783539e03eab976df7db4a
Holly Thomas <48989974+LittleBlue418@users.noreply.github.com>
2023-03-25 10:08:36 +0100

    Standard job (#4)
    * New scene to test quests
    
    * first draft of standard job
    
    * DialogueTree fixes
    
    - Better descriptions for InventoryContainsItemType and InventoryTakeItemOfType
    - Added AddItemToInventory
    
    * Standard job skeleton complete
    
    First full skeleton with sassy placeholder dialogue
    
    ---------
    
    Co-authored-by: Martin Parm <parmus@gmail.com>

--- Assets/_Project/Scripts/Quests/AddItemToInventory.cs
+++ Assets/_Project/Scripts/Quests/AddItemToInventory.cs
@@ -5,20 +5,20 @@ using ParadoxNotion.Design;
 namespace DualityGame.Quests{
 
 	[Category("Duality")]
-	[Description("Remove item from inventory")]
-	public class InventoryTakeItemOfType : ActionTask
+	[Description("Add item to inventory")]
+	public class AddItemToInventory : ActionTask
 	{
 		[RequiredField] public BBParameter<Inventory.Inventory> _inventory;
 		[RequiredField] public BBParameter<ItemType> _itemType;
 
-		protected override string info => _itemType.isNoneOrNull ? $"Remove item from inventory" : $"Remove {_itemType.value} from inventory";
+		protected override string info => _itemType.isNoneOrNull ? $"Add item to inventory" : $"Add {_itemType.value} to inventory";
 
 		//This is called once each time the task is enabled.
 		//Call EndAction() to mark the action as finished, either in success or failure.
 		//EndAction can be called from anywhere.
 		protected override void OnExecute()
 		{
-			var item = _inventory.value.RemoveItem(_itemType.value);
+			_inventory.value.AddItem(_itemType.value);
 			EndAction(true);
 		}
 
