4ce946a6f29eb6c0f22a748054b62411e24db12d
Martin Parm <parmus@gmail.com>
2023-05-14 13:03:19 +0200

    Work-around for properly initializing realm on load

---
+++ Assets/_Project/Scripts/Utilities/UnityLifetimeEvents.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace DualityGame.Utilities
+{
+    public class UnityLifetimeEvents : MonoBehaviour
+    {
+        [SerializeField] private UnityEvent _onAwake;
+        [SerializeField] private UnityEvent _onEnable;
+        [SerializeField] private UnityEvent _onDisable;
+        [SerializeField] private UnityEvent _onDestroy;
+
+        private void Awake() => _onAwake.Invoke();
+        private void OnEnable() => _onEnable.Invoke();
+        private void OnDisable() => _onDisable.Invoke();
+        private void OnDestroy() => _onDestroy.Invoke();
+    }
+}
