7223e6e7599b33b4c66d10ecdf546029a04a8376
Martin Parm <parmus@gmail.com>
2023-05-15 00:08:16 +0200

    Lots of menu styling

---
+++ Assets/_Project/Scripts/UI/ModalBackground.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace DualityGame.UI
+{
+    [RequireComponent(typeof(UIDocument))]
+    public class ModalBackground : MonoBehaviour
+    {
+        private VisualElement _background;
+
+        private void Awake()
+        {
+            var root = GetComponent<UIDocument>().rootVisualElement;
+            _background = root.Q<VisualElement>(className: "ModalBackground");
+        }
+
+        private void OnEnable() => _background.AddToClassList("Shown");
+        private void OnDisable() => _background.RemoveFromClassList("Shown");
+    }
+}
