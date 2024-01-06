18154ef17c18882b8e13e20d1231870e586db839
Martin Parm <parmus@gmail.com>
2024-01-06 01:14:46 +0100

    Minor Cleanup

---
+++ Assets/_Project/Scripts/Core/NotifierBase.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Games.GrumpyBear.Core.Observables.ScriptableObjects;
+using UnityEngine;
+
+namespace DualityGame.Core
+{
+    public class NotifierBase<T, TObservable> : MonoBehaviour where TObservable : Observable<T>
+    {
+        [SerializeField] private TObservable _observable;
+        [SerializeField] private bool _registerOnAwake;
+        private T _value;
+
+        private void Awake()
+        {
+            _value = GetComponent<T>();
+            if (!_registerOnAwake) return;
+            _observable.Set(_value);
+        }
+
+        private void OnEnable()
+        {
+            if (_registerOnAwake) return;
+            _observable.Set(_value);
+        }
+
+        private void OnDisable()
+        {
+            if (_registerOnAwake) return;
+            if (!EqualityComparer<T>.Default.Equals(_observable.Value, _value)) return;
+            _observable.Set(default);
+        }
+
+        private void OnDestroy()
+        {
+            if (!_registerOnAwake) return;
+            if (!EqualityComparer<T>.Default.Equals(_observable.Value, _value)) return;
+            _observable.Set(default);
+        }
+    }
+}
--- Assets/_Project/Scripts/Playables/PlayableDirectorNotifier.cs
+++ Assets/_Project/Scripts/Playables/PlayableDirectorNotifier.cs
@@ -1,40 +1,9 @@
-﻿using UnityEngine;
+﻿using DualityGame.Core;
+using UnityEngine;
 using UnityEngine.Playables;
 
 namespace DualityGame.Playables
 {
     [RequireComponent(typeof(PlayableDirector))]
-    public class PlayableDirectorNotifier : MonoBehaviour
-    {
-        [SerializeField] private PlayableDirectorObservable _playableDirectorObservable;
-        [SerializeField] private bool _registerOnAwake;
-        private PlayableDirector _playableDirector;
-
-        private void Awake()
-        {
-            _playableDirector = GetComponent<PlayableDirector>();
-            if (!_registerOnAwake) return;
-            _playableDirectorObservable.Set(_playableDirector);
-        }
-
-        private void OnEnable()
-        {
-            if (_registerOnAwake) return;
-            _playableDirectorObservable.Set(_playableDirector);
-        }
-
-        private void OnDisable()
-        {
-            if (_registerOnAwake) return;
-            if (_playableDirectorObservable.Value != _playableDirector) return;
-            _playableDirectorObservable.Set(null);
-        }
-
-        private void OnDestroy()
-        {
-            if (!_registerOnAwake) return;
-            if (_playableDirectorObservable.Value != _playableDirector) return;
-            _playableDirectorObservable.Set(null);
-        }
-    }
+    public class PlayableDirectorNotifier : NotifierBase<PlayableDirector, PlayableDirectorObservable> {}
 }
