94c10b93651e40c4212ede0cc5288ae09ebff63a
Martin Parm <parmus@gmail.com>
2024-03-23 10:39:40 +0100

    Fix editor bug when condition is unconfigured.

--- Assets/_Project/Scripts/Quests/Tasks/CheckQuestState.cs
+++ Assets/_Project/Scripts/Quests/Tasks/CheckQuestState.cs
@@ -13,7 +13,13 @@ namespace DualityGame.Quests.Tasks
         [RequiredField] public BBParameter<Quest> _quest;
         [RequiredField] public BBParameter<QuestLog.QuestState> _questState;
 
-        protected override string info => $"Quest state is {_questState.value}";
+        protected override string info => (_quest.isNoneOrNull, _questState.isNoneOrNull) switch
+        {
+            (true, true) => "Quest has some state",
+            (true, false) => $"Quest is {_questState.value}",
+            (false, true) => $"Quest '{_quest.value.name}' has some state",
+            (false, false) => $"Quest '{_quest.value.name}' is {_questState.value}",
+        };
 
         //Called once per frame while the condition is active.
         //Return whether the condition is success or failure.
@@ -23,6 +29,7 @@ namespace DualityGame.Quests.Tasks
             return entry != null && entry.State == _questState.value;
         }
 
-        public override void OnCreate(ITaskSystem ownerSystem) => _questLog.value = Resources.FindObjectsOfTypeAll<QuestLog>().FirstOrDefault();
+        public override void OnCreate(ITaskSystem ownerSystem) =>
+            _questLog.value = Resources.FindObjectsOfTypeAll<QuestLog>().FirstOrDefault();
     }
 }
