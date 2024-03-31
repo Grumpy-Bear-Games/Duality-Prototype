8a31501814eead481bd04a84f290e2ecf475b96b
Martin Parm <parmus@gmail.com>
2024-03-31 22:30:12 +0200

    Add editor-only hotkey for instantly dying
    This is useful for when encountering bugs

---
+++ Assets/_Project/Scripts/Utilities/HotkeyAction.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+namespace DualityGame.Utilities
+{
+    public class HotkeyAction : MonoBehaviour
+    {
+        [SerializeField] private InputActionReference _inputAction;
+        [SerializeField] private UnityEvent _action;
+
+        private void Awake()
+        {
+            _inputAction.action.performed += PerformAction;
+            _inputAction.action.Enable();
+        }
+
+        private void OnDestroy()
+        {
+            _inputAction.action.Disable();
+            _inputAction.action.performed -= PerformAction;
+        }
+
+        private void PerformAction(InputAction.CallbackContext obj) => _action?.Invoke();
+    }
+}
