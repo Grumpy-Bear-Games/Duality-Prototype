8b11d33f8f131567917aba572a8944c23e0d180d
Martin Parm <parmus@gmail.com>
2024-05-27 13:58:15 +0200

    Fix build-mode errors
    - Load SerializableScriptableObject on load
    - Load first scene group properly from main menu
    - Fix scene group in Theatre/Concert House
    - Remove old WebGL initialization scene from build
    - Remove ActorAssetEditor from build

---
+++ Assets/_Project/Scripts/Core/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using Games.GrumpyBear.Core.LevelManagement;
+using Games.GrumpyBear.Core.SaveSystem;
+using UnityEngine;
+
+namespace DualityGame.Core
+{
+    public class Bootstrapper: MonoBehaviour
+    {
+        [SerializeField] private SceneGroup _sceneGroup;
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void LoadAllSerializableScriptableObjects()
+        {
+            Resources.LoadAll<SerializableScriptableObject>("");
+        }
+
+        #if !UNITY_EDITOR
+        private IEnumerator Start()
+        {
+            yield return _sceneGroup.Load_CO();
+        }
+        #endif
+    }
+}
