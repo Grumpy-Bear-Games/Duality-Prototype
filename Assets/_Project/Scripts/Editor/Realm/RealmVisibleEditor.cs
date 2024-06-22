352366b8892608475bd977e98b7b2d00a4987dc2
Martin Parm <parmus@gmail.com>
2024-06-22 21:30:00 +0200

    Better RealmVisibleEditor

--- Assets/_Project/Scripts/Editor/Realm/RealmVisibleEditor.cs
+++ Assets/_Project/Scripts/Editor/Realm/RealmVisibleEditor.cs
@@ -13,7 +13,8 @@ namespace DualityGame.Editor.Realm
         private RealmVisible _realmVisible;
         private SerializedProperty _realmProperty;
 
-        private VisualElement _tools;
+        private VisualElement _misconfiguredLayersTools;
+        private VisualElement _realmNotConfiguredTools;
 
         private void OnEnable()
         {
@@ -32,8 +33,28 @@ namespace DualityGame.Editor.Realm
             var root = new VisualElement();
 
             InspectorElement.FillDefaultInspector(root, serializedObject, this);
-            _tools = new VisualElement();
-            root.Add(_tools);
+            root.Add(_misconfiguredLayersTools = new VisualElement());
+            _misconfiguredLayersTools.Add(new HelpBox
+            {
+                messageType = HelpBoxMessageType.Error,
+                text = "Some layers don't match Realm LevelLayer",
+            });
+            _misconfiguredLayersTools.Add(new Button(FixMisconfiguredLayers)
+            {
+                text = "Fix layers"
+            });
+            _misconfiguredLayersTools.Add(new Button(SelectMisconfiguredLayers)
+            {
+                text = "Select misconfigured layers"
+            });
+
+            root.Add(_realmNotConfiguredTools = new VisualElement());
+            _realmNotConfiguredTools.Add(new HelpBox
+            {
+                messageType = HelpBoxMessageType.Error,
+                text = "Realm must be set",
+            });
+            _realmNotConfiguredTools.Add(new Button(SetRealmAutomatically) { text = "Set Realm automatically" });
 
             UpdateTools();
             return root;
@@ -41,56 +62,56 @@ namespace DualityGame.Editor.Realm
 
         private void UpdateTools()
         {
-            _tools.Clear();
-            if (_realmProperty.objectReferenceValue is DualityGame.Realm.Realm realm)
+            if (_realmProperty.objectReferenceValue is not DualityGame.Realm.Realm realm)
             {
-                var misconfiguredLayers = _realmVisible
-                    .GetComponentsInChildren<Transform>(true)
-                    .Any(t => t.gameObject.layer != realm.LevelLayer);
-                if (!misconfiguredLayers) return;
-                _tools.Add(new HelpBox
-                {
-                    messageType = HelpBoxMessageType.Error,
-                    text = "Some layers don't match Realm LevelLayer",
-                });
-                _tools.Add(new Button(() =>
-                {
-                    Undo.SetCurrentGroupName("Fix layers");
-                    int undoGroup = Undo.GetCurrentGroup();
-                    foreach (var gameObject in _realmVisible
-                                 .GetComponentsInChildren<Transform>(true)
-                                 .Select(t => t.gameObject)
-                                 .Where(gameObject => gameObject.layer != realm.LevelLayer))
-                    {
-                        Undo.RecordObject(gameObject, "");
-                        gameObject.layer = realm.LevelLayer;
-                        EditorUtility.SetDirty(gameObject);
-                    }
-                    Undo.CollapseUndoOperations(undoGroup);
-                    UpdateTools();
-                })
-                {
-                    text = "Fix layers"
-                });
+                _realmNotConfiguredTools.style.display = DisplayStyle.Flex;
+                _misconfiguredLayersTools.style.display = DisplayStyle.None;
+                return;
             }
-            else
+
+            _realmNotConfiguredTools.style.display = DisplayStyle.None;
+            var misconfiguredLayers = _realmVisible
+                .GetComponentsInChildren<Transform>(true)
+                .Any(t => t.gameObject.layer != realm.LevelLayer);
+            _misconfiguredLayersTools.style.display = misconfiguredLayers ? DisplayStyle.Flex : DisplayStyle.None;
+        }
+
+        private void SetRealmAutomatically()
+        {
+            serializedObject.UpdateIfRequiredOrScript();
+            _realmProperty.objectReferenceValue = DualityGame.Realm.Realm.FromLayer(_realmVisible.gameObject.layer);
+            serializedObject.ApplyModifiedProperties();
+            UpdateTools();
+        }
+
+        private void FixMisconfiguredLayers()
+        {
+            if (_realmProperty.objectReferenceValue is not DualityGame.Realm.Realm realm) return;
+
+            Undo.SetCurrentGroupName("Fix layers");
+            var undoGroup = Undo.GetCurrentGroup();
+            foreach (var gameObject in _realmVisible
+                         .GetComponentsInChildren<Transform>(true)
+                         .Select(t => t.gameObject)
+                         .Where(gameObject => gameObject.layer != realm.LevelLayer))
             {
-                _tools.Add(new HelpBox
-                {
-                    messageType = HelpBoxMessageType.Error,
-                    text = "Realm must be set",
-                });
-                _tools.Add(new Button(() =>
-                {
-                    serializedObject.UpdateIfRequiredOrScript();
-                    _realmProperty.objectReferenceValue = DualityGame.Realm.Realm.FromLayer(_realmVisible.gameObject.layer);
-                    serializedObject.ApplyModifiedProperties();
-                    UpdateTools();
-                })
-                {
-                    text = "Set Realm automatically"
-                });
+                Undo.RecordObject(gameObject, "");
+                gameObject.layer = realm.LevelLayer;
+                EditorUtility.SetDirty(gameObject);
             }
+            Undo.CollapseUndoOperations(undoGroup);
+            UpdateTools();
+        }
+
+        private void SelectMisconfiguredLayers()
+        {
+            if (_realmProperty.objectReferenceValue is not DualityGame.Realm.Realm realm) return;
+
+            Selection.objects = _realmVisible
+                .GetComponentsInChildren<Transform>(true)
+                .Select(t => t.gameObject)
+                .Where(gameObject => gameObject.layer != realm.LevelLayer)
+                .ToArray();
         }
     }
 }
