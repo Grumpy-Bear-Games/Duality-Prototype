f1ddf0fc401c207c90a4e4551e0010e57a98cb98
Martin Parm <parmus@gmail.com>
2025-06-21 22:26:33 +0200

    Complete SpriteController

--- Assets/_Project/Scripts/Warp/SpriteController.cs
+++ Assets/_Project/Scripts/Warp/SpriteController.cs
@@ -1,37 +1,46 @@
 ﻿using System;
-using System.Collections;
-using UnityEditor;
+using Games.GrumpyBear.Core.SaveSystem;
 using UnityEngine;
 
 namespace DualityGame.Warp
 {
     [RequireComponent(typeof(SpriteRenderer))]
-    public class SpriteController : MonoBehaviour
+    public class SpriteController : MonoBehaviour, ISaveableComponent
     {
-        [SerializeField] private bool _outline;
+        private static readonly int OutlineEnabledProperty = Shader.PropertyToID("_Outline_Enabled");
+
+        [SerializeField] private bool _outline = true;
 
         private SpriteRenderer _spriteRenderer;
+        private MaterialPropertyBlock _mpb;
 
         public bool Outline
         {
             get => _outline;
             set {
                 _outline = value;
+                UpdateMaterialPropertyBlock();
             }
         }
 
-        private void Awake()
+        private void UpdateMaterialPropertyBlock()
+        {
+            _mpb.Clear();
+            _mpb.SetFloat(OutlineEnabledProperty, _outline ? 1 : 0);
+            _spriteRenderer.SetPropertyBlock(_mpb);
+        }
+
+        public void Awake()
         {
             _spriteRenderer = GetComponent<SpriteRenderer>();
+            _mpb = new MaterialPropertyBlock();
         }
 
-        #if UNITY_EDITOR
-        private void OnValidate()
-        {
-            //if (EditorApplication.isPlayingOrWillChangePlaymode) ApplyMaterialProperties();
-        }
+        private void Start() => UpdateMaterialPropertyBlock();
 
-        private void Reset() => _spriteRenderer = GetComponent<SpriteRenderer>();
-        #endif
+        #region ISavableComponent
+        object ISaveableComponent.CaptureState() => _outline;
+        void ISaveableComponent.RestoreState(object state) => Outline = (bool)state;
+        #endregion
     }
 }
