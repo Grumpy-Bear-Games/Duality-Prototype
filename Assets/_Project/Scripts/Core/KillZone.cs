c51cf11b2553b58f7de61af93fc6cf84c175dfa7
Martin Parm <parmus@gmail.com>
2025-06-22 20:28:24 +0200

    Restored KillZone (accidental deletion)

---
+++ Assets/_Project/Scripts/Core/KillZone.cs
@@ -0,0 +1,16 @@
+﻿using DualityGame.Player;
+using UnityEngine;
+
+namespace DualityGame.Core
+{
+    public class KillZone : MonoBehaviour
+    {
+        [SerializeField] private CauseOfDeath _causeOfDeath;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            var killable = other.GetComponent<IKillable>();
+            killable?.Kill(_causeOfDeath);
+        }
+    }
+}
